60191de26eba29891e948e6e9436c86dff594641
Dimitar Haralanov <dimitar@forebits.com>
2013-03-23 16:14:21 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githubstats::Application.config.secret_token = 'a4a41375bd8fac31f9366b649230949cab86bd3e03ebb48f2717980ebb350c6009ba3b033e1558afee6a84a6ed75393a6adf01588325c29c6bb2eb16b2443ae5'
